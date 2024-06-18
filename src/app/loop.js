3e52427ae8cbf0c6936d3037fdef2c5eb9e70449
kevvj <kevinjvillalba774+1@gmail.com>
2024-06-18 01:02:56 -0500

    1

--- src/app/loop.js
+++ src/app/loop.js
@@ -1,11 +1,16 @@
-
-
-
-export default function InfiniteLoop({ array }) {
+const ILoop = ()=> {
     return (
-      <div>
-
+      <div class = "">
+        <div>
+            <div><span>#</span>1</div>
+            <div><span>#</span>2</div>
+            <div><span>#</span>3</div>
+            <div><span>#</span>4</div>
+            <div><span>#</span>5</div>
+        </div>
       </div>
     );
   }
+
+  export default ILoop
   
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,9 @@
 import Image from "next/image";
+import Loop from "./loop";
 
 export default function Home() {
   return (<div>
-    
+    <Loop></Loop>
 
   </div>)
     

3d21b6bdfb019ab10d7f9678a1356320fa02dfcf
kevvj <kevinjvillalba774+1@gmail.com>
2024-08-29 02:17:53 -0500

    changestykes

--- src/app/loop.js
+++ src/app/loop.js
@@ -1,13 +1,23 @@
 const ILoop = ()=> {
     return (
-      <div class = "">
-        <div>
-            <div><span>#</span>1</div>
-            <div><span>#</span>2</div>
-            <div><span>#</span>3</div>
-            <div><span>#</span>4</div>
-            <div><span>#</span>5</div>
+      <div className = "tag-list">
+        <div className="inner">
+            <div className="tag"><span>#</span>maldita sea</div>
+            <div className="tag"><span>#</span>maldita sea</div>
+            <div className="tag"><span>#</span>maldita sea</div>
+            <div className="tag"><span>#</span>maldita sea</div>
+            <div className="tag"><span>#</span>maldita sea</div>
+            <div className="tag"><span>#</span>maldita sea</div>
+            <div className="tag"><span>#</span>maldita sea</div>
+            <div className="tag"><span>#</span>maldita sea</div>
+            <div className="tag"><span>#</span>maldita sea</div>
+            <div className="tag"><span>#</span>maldita sea</div>
+            <div className="tag"><span>#</span>maldita sea</div>
+            <div className="tag"><span>#</span>maldita sea</div>
+            <div className="tag"><span>#</span>maldita sea</div>
+            <div className="tag"><span>#</span>maldita sea</div>
         </div>
+        <div className="fade"></div>
       </div>
     );
   }
--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Image from "next/image";
 import Loop from "./loop";
 import Header from "./Header";
@@ -5,13 +6,20 @@ import Menu from "./Menu";
 import Principal from "./PrincipalContent";
 
 export default function Home() {
-  return (<div>
-    {/* <Loop></Loop> */}
-
-    <Header></Header>
-    <Menu></Menu>
-    <Principal></Principal>
-
-  </div>)
-    
+  return (
+    <div className="container">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Header />
+      <Menu />
+      <Principal />
+      <Loop />
+    </div>
+  );
 }

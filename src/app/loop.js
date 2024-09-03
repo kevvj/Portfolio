dc2d50b7d7b0222575f35f789be45ea24529a837
kevvj <kevinjvillalba774+1@gmail.com>
2024-09-03 00:21:59 -0500

    muchascosasssaasdasdwdw

--- src/app/loop.js
+++ src/app/loop.js
@@ -1,23 +1,66 @@
 const ILoop = ()=> {
     return (
-      <div className = "tag-list">
-        <div className="inner">
-            <div className="tag"><span>#</span>maldita sea</div>
-            <div className="tag"><span>#</span>maldita sea</div>
-            <div className="tag"><span>#</span>maldita sea</div>
-            <div className="tag"><span>#</span>maldita sea</div>
-            <div className="tag"><span>#</span>maldita sea</div>
-            <div className="tag"><span>#</span>maldita sea</div>
-            <div className="tag"><span>#</span>maldita sea</div>
-            <div className="tag"><span>#</span>maldita sea</div>
-            <div className="tag"><span>#</span>maldita sea</div>
-            <div className="tag"><span>#</span>maldita sea</div>
-            <div className="tag"><span>#</span>maldita sea</div>
-            <div className="tag"><span>#</span>maldita sea</div>
-            <div className="tag"><span>#</span>maldita sea</div>
-            <div className="tag"><span>#</span>maldita sea</div>
+      <div className = "slider">
+        <div className = "slide-track">
+
+          <div className = "slide">
+            <img src = "/img/react.png" alt = ""></img>
+          </div>
+
+          <div className = "slide">
+          <img src = "/img/Angular.png" alt = ""></img>
+          </div>
+
+          <div className = "slide">
+            <img src ="/img/Tailwind.png" alt = ""></img>
+          </div>
+
+          <div className = "slide">
+            <img src ="/img/Next.png" alt = ""></img>
+          </div>
+
+          <div className = "slide">
+          <img src ="/img/mysql.png" alt = ""></img>
+          </div>
+
+          <div className = "slide">
+            <img src ="https://static.vecteezy.com/system/resources/previews/027/214/398/original/aubergine-aubergine-transparent-background-ai-generated-free-png.png" alt = ""></img>
+          </div>
+
+          <div className = "slide">
+            <img src ="https://static.vecteezy.com/system/resources/previews/027/214/398/original/aubergine-aubergine-transparent-background-ai-generated-free-png.png" alt = ""></img>
+          </div>
+
+
+
+          <div className = "slide">
+          <img src = "/img/react.png" alt = ""></img>
+          </div>
+
+          <div className = "slide">
+          <img src = "/img/Angular.png" alt = ""></img>
+          </div>
+
+          <div className = "slide">
+          <img src ="/img/Tailwind.png" alt = ""></img>
+          </div>
+
+          <div className = "slide">
+            <img src ="img/Next.png" alt = ""></img>
+          </div>
+
+          <div className = "slide">
+            <img src ="/img/mysql.png" alt = ""></img>
+          </div>
+
+          <div className = "slide">
+            <img src ="https://static.vecteezy.com/system/resources/previews/027/214/398/original/aubergine-aubergine-transparent-background-ai-generated-free-png.png" alt = ""></img>
+          </div>
+
+          <div className = "slide">
+            <img src ="https://static.vecteezy.com/system/resources/previews/027/214/398/original/aubergine-aubergine-transparent-background-ai-generated-free-png.png" alt = ""></img>
+          </div>
         </div>
-        <div className="fade"></div>
       </div>
     );
   }

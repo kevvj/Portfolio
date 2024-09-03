d5c7296bb009380bb847e1d8fbd1c0b1a5994de5
kevvj <kevinjvillalba774+1@gmail.com>
2024-09-03 00:38:07 -0500

    v

--- src/app/loop.js
+++ src/app/loop.js
@@ -28,7 +28,7 @@ const ILoop = ()=> {
           </div>
 
           <div className = "slide">
-            <img src ="https://static.vecteezy.com/system/resources/previews/027/214/398/original/aubergine-aubergine-transparent-background-ai-generated-free-png.png" alt = ""></img>
+            <img src ="https://1000marcas.net/wp-content/uploads/2023/11/Emoji-Peach.png" alt = ""></img>
           </div>
 
 
@@ -58,7 +58,7 @@ const ILoop = ()=> {
           </div>
 
           <div className = "slide">
-            <img src ="https://static.vecteezy.com/system/resources/previews/027/214/398/original/aubergine-aubergine-transparent-background-ai-generated-free-png.png" alt = ""></img>
+            <img src ="https://1000marcas.net/wp-content/uploads/2023/11/Emoji-Peach.png" alt = ""></img>
           </div>
         </div>
       </div>

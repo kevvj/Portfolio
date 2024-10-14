9958918725c96fe7062023fdf5d032880b0585f0
kevvj <kevinjvillalba774+1@gmail.com>
2024-10-14 00:15:17 -0500

    a

--- src/app/Header.js
+++ src/app/Header.js
@@ -3,14 +3,20 @@ import Menu from "./Menu";
 const Header = () => {
     return (
         <div className="Header">
-            <header>
-            <img className="top-pic" src="https://content.imageresizer.com/images/memes/El-Gato-meme-6.jpg"></img>
-            </header>
+
+            <div className="pic-container"> 
+                <img className="top-pic" src="https://content.imageresizer.com/images/memes/El-Gato-meme-6.jpg"></img>
+
+                <p>No se que poner aqui</p>
+            </div>
+
+            {/* <h1>KEVIN TAPIAS</h1> */}
+
 
             <Menu></Menu>
         </div>
 
-        
+
     );
 }
 

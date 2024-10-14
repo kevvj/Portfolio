09d2a7f6e2c4b3f9e6aab9b198868a8457346b31
kevvj <kevinjvillalba774+1@gmail.com>
2024-10-13 23:32:32 -0500

    new

--- src/app/Menu.js
+++ src/app/Menu.js
@@ -1,54 +1,26 @@
 'use client';
 
-const scrollToElement = (element, duration) => {
-    const start = window.scrollY;
-    const targetPosition = element.getBoundingClientRect().top + window.scrollY;
-    const distance = targetPosition - start;
-    let startTime = null;
 
-    const animation = (currentTime) => {
-        if (startTime === null) startTime = currentTime;
-        const timeElapsed = currentTime - startTime;
-        const progress = Math.min(timeElapsed / duration, 1); // Normaliza el progreso a un rango de 0 a 1
-
-        window.scrollTo(0, start + distance * easeInOutQuad(progress));
-
-        if (timeElapsed < duration) {
-            requestAnimationFrame(animation);
-        }
-    };
-
-    const easeInOutQuad = (t) => {
-        return t < 0.5
-            ? 2 * t * t
-            : -1 + (4 - 2 * t) * t;
-    };
-
-    requestAnimationFrame(animation);
-};
-
+ 
 
 const Menu = () => {
 
-    const handleScroll = (event, sectionId) => {
-        event.preventDefault();
-        const element = document.getElementById(sectionId);
-        if (element) {
-            scrollToElement(element, 1000); // Duración de la animación en ms
-        }
-    };
+    
 
     return (
         <div className="main-menu">
             <ul>
                 <li>
-                    <a href="#aboutme" onClick={(e) => handleScroll(e, 'aboutme')}>About me</a>
+                    <a href="">Home</a>
                 </li>
                 <li>
-                    <a href="#proyects" onClick={(e) => handleScroll(e, 'proyects')}>Proyects</a>
+                    <a href="">Proyects</a>
                 </li>
                 <li>
-                    <a href="#experience" onClick={(e) => handleScroll(e, 'experience')}>Experience</a>
+                    <a href="">Skills</a>
+                </li>
+                <li>
+                    <a href="">Contact</a>
                 </li>
             </ul>
         </div>
--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,15 @@
-import Head from 'next/head';
-import Image from "next/image";
-import Loop from "./loop";
+
 import Header from "./Header";
 import Menu from "./Menu";
 import Principal from "./PrincipalContent";
-import Proyect from './Proyects';
 
 export default function Home() {
   return (
     <div className="container">
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
+      
       <Header />
       <Menu />
       <Principal />
-      <Proyect />
-      <Loop />
       
     </div>
   );
